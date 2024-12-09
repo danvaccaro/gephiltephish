189df630958542abee04642bbbda3be5d8ba0fce
danvaccaro <83193983+danvaccaro@users.noreply.github.com>
2024-12-09 18:41:54 -0500

    api url is now env var

--- frontend/gephiltephish/app/config/api.ts
+++ frontend/gephiltephish/app/config/api.ts
@@ -22,7 +22,7 @@ export class DuplicateVoteError extends ApiError {
     }
 }
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api';
 
 export const API_ENDPOINTS = {
     LOGIN: `${API_BASE_URL}/login/`,
